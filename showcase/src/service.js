1bdb5d4514bc8452c58714c55e65ff338403361d
jtulk <jtulk@turner-industries.com>
2020-05-27 16:29:28 -0500

    feat: adds basic toast implementation
    fix: icons not rendering correctly
    chore: removed unnecessary component

--- showcase/src/service.js
+++ showcase/src/service.js
@@ -1,57 +1,97 @@
-const toasts = {};
-let count = 1;
+const types = {
+  error: "error",
+  warning: "warning",
+  success: "success",
+  info: "info",
+};
+
+const icons = {
+  [types.error]: "exclamation triangle",
+  [types.info]: "question",
+  [types.success]: "check",
+  [types.warning]: "exclamation triangle",
+};
+
 const showToast = (text) => {
-  var toastKey = addToastToContainer(text);
+  const toastKey = addToastToContainer(text);
+  trackAndRemoveToast(toastKey);
+};
+
+const trackAndRemoveToast = (key) => {
+  const container = document.body.querySelector(".toast-container");
+  const toast = document.body.querySelector(`#toast${key}`);
 
   setTimeout(() => {
-    console.log("removing toast", toastKey);
-    removeToast(toastKey);
+    if (!toast.matches(":hover")) {
+      toast.classList.toggle("active");
+      toast.style.opacity = 0;
+
+      setTimeout(() => {
+        container.removeChild(toast);
+      }, 200);
+    } else {
+      trackAndRemoveToast(key);
+    }
   }, 2000);
 };
 
 const addToastToContainer = (options) => {
   const container = getOrCreateContainer();
-  const key = count++;
+
   const toastOptions = {
-    key,
+    key: Date.now(),
     ...options,
   };
 
   const toast = createToast(toastOptions);
 
   container.appendChild(toast);
+
   setTimeout(() => {
     toast.classList.toggle("active");
     toast.style.opacity = 1;
   }, 1);
 
-  return key;
+  return toastOptions.key;
 };
 
 const createToast = (options) => {
+  const icon =
+    options.showIcon && icons[options.type] ? icons[options.type] : "";
+
   const toast = document.createElement("div");
-  toast.className = `ui toast ${options.type} message appearing`;
-  toast.innerText = options.text;
+
+  toast.className = `ui ${options.type} ${
+    !!icon ? "icon" : ""
+  } message active toast`;
+
+  const header = options.header
+    ? `<div class="header">${options.header}</div>`
+    : "";
+
+  const body = options.text ? `<p>${options.text}</p>` : "";
+
+  const toastIcon = icon
+    ? `<i aria-hidden="true" class="${icon} icon"></i>`
+    : "";
+
+  const closeIcon = options.closeable ? '<i class="close icon" ></i>' : "";
+  const nestContent = !!icon || !!header;
+
+  toast.innerHTML = `${toastIcon} ${
+    nestContent ? '<div class="content">' : ""
+  } ${closeIcon} ${header} ${body}${nestContent ? "</div>" : ""}`;
+
   toast.setAttribute("id", `toast${options.key}`);
   toast.style.opacity = 0;
 
   return toast;
-};
-const removeToast = (key) => {
-  const container = document.body.querySelector(".toast-container");
-  const toast = document.body.querySelector(`#toast${key}`);
-  toast.classList.toggle("active");
-  toast.style.opacity = 0;
-  setTimeout(() => {
-    container.removeChild(toast);
-  }, 200);
 };
 
 const getOrCreateContainer = () => {
   let container = document.querySelector(".toast-container");
 
   if (!container) {
-    console.log("creating container");
     container = document.createElement("div");
     container.classList.add("toast-container");
   }
@@ -61,24 +101,20 @@ const getOrCreateContainer = () => {
   return container;
 };
 
-const types = {
-  error: "error",
-  warning: "warning",
-  success: "success",
-  info: "info",
-};
-
 export const toastService = {
-  success: (text) => {
-    showToast({ type: types.success, text });
+  showToast: (text, options = {}) => {
+    showToast({ ...options, type: "", text });
   },
-  warning: (text) => {
-    showToast({ type: types.warning, text });
+  success: (text, options = {}) => {
+    showToast({ ...options, type: types.success, text });
   },
-  error: (text) => {
-    showToast({ type: types.error, text });
+  warning: (text, options = {}) => {
+    showToast({ ...options, type: types.warning, text });
   },
-  info: (text) => {
-    showToast({ type: types.info, text });
+  error: (text, options = {}) => {
+    showToast({ ...options, type: types.error, text });
+  },
+  info: (text, options = {}) => {
+    showToast({ ...options, type: types.info, text });
   },
 };
